7915a90f89400c1cde1b075850bac9593aab5483
fingla <felixinglavf@gmail.com>
2023-05-01 14:31:30 +0200

    swap-color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function ImageUpload() {
+  const [image, setImage] = useState(null);
+
+  const handleImageUpload = (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
+    reader.onload = () => {
+      const img = new Image();
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < imageData.data.length; i += 4) {
+          if (imageData.data[i] === 255 && imageData.data[i + 1] === 255 && imageData.data[i + 2] === 255) {
+            imageData.data[i] = 0;
+            imageData.data[i + 1] = 0;
+            imageData.data[i + 2] = 255;
+          }
+        }
+        ctx.putImageData(imageData, 0, 0);
+        const modifiedImage = canvas.toDataURL('image/jpeg');
+        setImage(modifiedImage);
+      };
+      img.src = reader.result;
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" onChange={handleImageUpload} />
+      {image && <img src={image} alt="Uploaded and modified" />}
     </div>
   );
 }
 
-export default App;
+export default ImageUpload;
